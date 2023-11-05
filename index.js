c273a5e8216dfc14a29d5b78b1d3a4cf409ae11b
malaniharshita <malaniharshita>
2023-11-05 21:41:03 +0530

    taskDone

---
+++ index.js
@@ -0,0 +1,8 @@
+// let items1 = document.getElementsByClassName('list-group-item');
+// for(let i = 0; i<items1.length; i++){
+//     items1[i].style.backgroundColor = 'green';
+// }
+let items = document.getElementsByTagName('li');
+for(let i = 0; i<items.length; i++){
+    items[i].style.backgroundColor = 'green';
+}
